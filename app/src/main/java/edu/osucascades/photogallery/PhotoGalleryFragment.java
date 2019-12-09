de343d28c7f4eff346f634ca0733ce0d674d62e1
mwinb <mwinberry0101@gmail.com>
2019-12-08 19:36:59 -0800

    Finished challenge 3

--- app/src/main/java/edu/osucascades/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/edu/osucascades/photogallery/PhotoGalleryFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +20,7 @@ public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
     private List<GalleryItem> mItems = new ArrayList<>();
     private int mCurrentPage;
+    private int mNumColumns = 3;
 
 
     private RecyclerView mPhotoRecyclerView;
@@ -39,7 +41,6 @@ public class PhotoGalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
-        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
@@ -52,7 +53,25 @@ public class PhotoGalleryFragment extends Fragment {
                 }
             }
         });
+        mPhotoRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int numberOfColumns = 1;
+                int width = mPhotoRecyclerView.getWidth();
+                int widthOfSingleElement = 250;
+                if(width > widthOfSingleElement) {
+                    numberOfColumns = width/widthOfSingleElement;
+                }
+                else {
+                    numberOfColumns = 3;
+                }
+                mNumColumns = numberOfColumns;
+            }
+        });
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), mNumColumns));
+
         setupAdapter();
+
 
         return v;
     }
